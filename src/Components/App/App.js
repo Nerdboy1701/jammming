f265a06cdc25b2896c503a5901f94fcd5c246fd2
nerdboy <saintofsteel@me.com>
2020-11-08 18:04:32 -0500

    Render Hard code Tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
-import './App.css';
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+import "./App.css";
 
- class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-          <SearchBar></SearchBar>
-          <div className="App-playlist">
-            <SearchResults ></SearchResults>
-            <Playlist></Playlist>
-          </div>
-        </div>
-      </div>
-    );
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
+      ]
+    }
   }
- }
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className="highlight">mmm</span>ing
+				</h1>
+				<div className="App">
+					<SearchBar></SearchBar>
+					<div className="App-playlist">
+						<SearchResults searchResults={this.state.searchResults}></SearchResults>
+						<Playlist></Playlist>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <button className="Track-action">-</button>
+        } else {
+            return <button className="Track-action">+</button>
+        }
+    }
 
     render() {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>Track Name</h3>
-                    <p>Track Artist | Track Album</p>
+                <h3>{this.props.track.name}</h3>
+                <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
-        <button className="Track-action"></button>
+                {this.renderAction()}
             </div>
         );
     }
