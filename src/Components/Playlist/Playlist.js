527855b963a91e53d3d0923d714b9fa1c65e9576
nerdboy <saintofsteel@me.com>
2020-11-15 09:11:49 -0500

    Playlist Name Not Changing Fixes #3 = Fixed

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -13,12 +13,11 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}/>
+                <input defaultValue="New Playlist" onChange={this.handleNameChange}/>
                 <TrackList 
                     tracks={this.props.playlistTracks} 
                     onRemove={this.props.onRemove} 
-                    isRemoval={true} 
-                    onChange={this.handleNameChange}
+                    isRemoval={true}       
                 ></TrackList>
                 <button className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</button>
             </div>
